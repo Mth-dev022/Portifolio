4b3e3dd3df21df618b7639d4318c1547ec77e799
Matheus Souza <Matheuszebende6@gmail.com>
2024-03-24 16:03:21 -0300

    Delete src directory

--- src/components/Projects/Components/DesenvolvimentoComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DesenvolvimentoComponent = () => {
-  return (
-    <div style={{marginTop:'50px'}}>Sem projetos...</div>
-  )
-}
-
-export default DesenvolvimentoComponent
